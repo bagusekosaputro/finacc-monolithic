a1ba0fc2fa3e50b6a943e1c1c46a685113b82ce7
Bagus ES <bestbuper14@gmail.com>
2020-03-29 18:08:01 +0700

    Revamp register and login page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,35 +1,110 @@
-@extends('layout.default')
-@section('content')
-<section class="hero-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 hero-text">
-				<h2>Catat <span>Pengeluaranmu</span> <br>dengan mudah</h2>
-                <h4>Aplikasi Web untuk mencatat pengeluaran harianmu</h4>
-                @if ($message = Session::get('error'))
-                    <div class="alert alert-danger alert-block">
-                        <button type="button" class="close" data-dismiss="alert">×</button> 
-                        <strong>{{ $message }}</strong>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Hitungin - Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="{{ asset('/auth/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="{{ asset('/auth/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    @if ($message = Session::get('error'))
+                                        <div class="alert alert-danger alert-block">
+                                            <button type="button" class="close" data-dismiss="alert">×</button> 
+                                            <strong>{{ $message }}</strong>
+                                        </div>
+                                    @endif
+                                    <form class="user" method="POST" action="{{ url('login') }}">
+                                        {{ csrf_field() }}
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                            @if ($errors->has('email'))
+                                                <span class="error">{{ $errors->first('email') }}</span>
+                                            @endif  
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                            @if ($errors->has('password'))
+                                                <span class="error">{{ $errors->first('password') }}</span>
+                                            @endif
+                                        </div>
+                                        <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                            <label class="custom-control-label" for="customCheck">Remember Me</label>
+                                        </div>
+                                        </div>
+                                        <button class="btn btn-primary btn-user btn-block">
+                                        Login
+                                        </button>
+                                        <!-- <hr> -->
+                                        <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a> -->
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="{{ url('register') }}">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                @endif
-				<form class="hero-subscribe-from" method="POST" action="{{ url('login') }}">
-                    {{ csrf_field() }}
-                    <input type="email" name="email" placeholder="Enter your email">
-                    @if ($errors->has('email'))
-                        <span class="error">{{ $errors->first('email') }}</span>
-                    @endif  
-                    <input type="password" name="password" placeholder="Enter your password">
-                    @if ($errors->has('password'))
-                        <span class="error">{{ $errors->first('password') }}</span>
-                    @endif
-                    <br>
-					<a href="{{url('register')}}">Daftar</a> <button class="site-btn sb-gradients pull-right">Masuk</button>
-				</form>
-			</div>
-			<div class="col-md-6">
-				<img src="img/laptop.png" class="laptop-image" alt="">
-			</div>
-		</div>
-	</div>
-</section>
-@stop
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{ asset('/auth/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('/auth/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{ asset('/auth/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{ asset('/auth/js/sb-admin-2.min.js') }}"></script>
+
+</body>
+
+</html>
--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -1,35 +1,108 @@
-@extends('layout.default')
-@section('content')
-<section class="hero-section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 hero-text">
-				<h2>Catat <span>Pengeluaranmu</span> <br>dengan mudah</h2>
-                <h4>Aplikasi Web untuk mencatat pengeluaran harianmu</h4>
-                @if ($message = Session::get('error'))
-                    <div class="alert alert-danger alert-block">
-                        <button type="button" class="close" data-dismiss="alert">×</button> 
-                        <strong>{{ $message }}</strong>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Hitungin - Register</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="{{ asset('/auth/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="{{ asset('/auth/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            @if ($message = Session::get('error'))
+                                <div class="alert alert-danger alert-block">
+                                    <button type="button" class="close" data-dismiss="alert">×</button> 
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @endif
+                            <form class="user" method="POST" action="{{ url('register') }}">
+                                {{ csrf_field() }}
+                                <div class="form-group row">
+                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
+                                </div>
+                                </div>
+                                <div class="form-group">
+                                <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                    @if ($errors->has('email'))
+                                        <span class="error">{{ $errors->first('email') }}</span>
+                                    @endif 
+                                </div>
+                                <div class="form-group row">
+                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                    @if ($errors->has('password'))
+                                        <span class="error">{{ $errors->first('password') }}</span>
+                                    @endif  
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                                </div>
+                                </div>
+                                <button class="btn btn-primary btn-user btn-block">
+                                Register Account
+                                </button>
+                                <!-- <hr> -->
+                                <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                                <i class="fab fa-google fa-fw"></i> Register with Google
+                                </a>
+                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                </a> -->
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="{{ url('login') }}">Already have an account? Login!</a>
+                            </div>
+                        </div>
                     </div>
-                @endif
-				<form class="hero-subscribe-from" method="POST" action="{{ url('register') }}">
-                    {{ csrf_field() }}
-                    <input type="email" name="email" placeholder="Enter your email">
-                    @if ($errors->has('email'))
-                        <span class="error">{{ $errors->first('email') }}</span>
-                    @endif  
-                    <input type="password" name="password" placeholder="Enter your password">
-                    @if ($errors->has('password'))
-                        <span class="error">{{ $errors->first('password') }}</span>
-                    @endif
-                    <br>
-					Sudah <a href="{{url('login')}}">terdaftar</a> <button class="site-btn sb-gradients pull-right">Daftar</button>
-				</form>
-			</div>
-			<div class="col-md-6">
-				<img src="img/laptop.png" class="laptop-image" alt="">
-			</div>
-		</div>
-	</div>
-</section>
-@stop
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{ asset('/auth/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('/auth/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{ asset('/auth/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{ asset('/auth/js/sb-admin-2.min.js') }}"></script>
+
+</body>
+
+</html>
